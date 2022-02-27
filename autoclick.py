172c953467d2a6e0680f34c700fc78f0cc53264f
zedsousa <hyuji125@gmail.com>
2022-02-27 14:46:17 -0300

    Adicionando arquivo de configuração

--- autoclick.py
+++ autoclick.py
@@ -1,38 +1,18 @@
 import time
-from mouse import clickBtn
+import yaml
 import pyautogui
+from mouse import clickBtn
 from images import load_images
 from screen import positions
 
 targets = load_images(dir_path='targets/')
-def minutes(seconds):
-    return seconds*60
-
-def raceHorse():
-    time.sleep(10)
-    while True:
-        while True:
-            if(clickBtn(targets['start'])):
-                break
-        while True:
-            if(clickBtn(targets['solicitacao'])):
-                break
-        while True:
-            if(clickBtn(targets['assinar'])):
-                break
-        time.sleep(5)    
-        while True:
-            if(clickBtn(targets['atividade'])):
-                break    
-        time.sleep(80)    
-        pyautogui.moveTo(372,620,1)
-        pyautogui.click() 
-
+stream = open("config.yaml", 'r')
+c = yaml.safe_load(stream)
 
 
 def rentHorse():
     time.sleep(10)
-    horseNumber = 1688601
+    horseNumber = c['horse_number']
     while True:
         pyautogui.hotkey('ctrl','t')
         url = 'https://play.pegaxy.io/renting/listing/'+str(horseNumber)
@@ -40,35 +20,36 @@ def rentHorse():
         pyautogui.hotkey('enter')
         time.sleep(5)
         if(len(positions(targets['with-horse']))==0):
+            start = time.time()
             while True:
                 pyautogui.hotkey('f5')
                 time.sleep(2)
+                now = time.time()
+                if (now - start > c['time_out']):
+                    break
 
                 if(len(positions(targets['with-horse']))>0):
-                    if(len(positions(targets['10%']))>0):
-                        if(clickBtn(targets['first-rent'])):
-                            while True:
-                                if(clickBtn(targets['captcha'])):
-                                    break
+                
+                    if(clickBtn(targets['first-rent'])):
+                        while True:
+                            if(clickBtn(targets['captcha'])):
+                                break
 
-                            pyautogui.moveTo(440,471,1)
-                            pyautogui.click()    
-                            
-                            while True:
-                                if(clickBtn(targets['desaprovado'])):
-                                    break
-                            while True:
-                                if(clickBtn(targets['confirmar'])):
-                                    break
-                            time.sleep(5)  
-                            pyautogui.moveTo(480,454,1)
-                            pyautogui.click()  
-                            while True:
-                                if(clickBtn(targets['atividade'])):
-                                    break
-                            time.sleep(5)    
-                            pyautogui.moveTo(581,295,1)
-                            pyautogui.click()    
+                        pyautogui.moveTo(c['rent_x'],c['rent_y'],1)
+                        pyautogui.click()    
+                        
+                        while True:
+                            if(clickBtn(targets['desaprovado'])):
+                                break
+                        while True:
+                            if(clickBtn(targets['confirmar'])):
+                                break 
+                        while True:
+                            if(clickBtn(targets['atividade'])):
+                                break
+                        time.sleep(5)    
+                        pyautogui.moveTo(c['close_x'],c['close_y'],1)
+                        pyautogui.click()    
                     break
                 
         pyautogui.hotkey('ctrl','w')
--- screen.py
+++ screen.py
@@ -7,13 +7,13 @@ def screenShot(monitor_number):
     with mss.mss() as sct:
         monitor = sct.monitors[monitor_number-1]
         sct_img = np.array(sct.grab(monitor))
-
+        
         return sct_img[:,:,:3]
 
 def positions(target, threshold=0.7,img = None):
     #Returns a list of positions of a target within an image
     if img is None:
-        img = screenShot(2)
+        img = screenShot(1)
 
     result = cv2.matchTemplate(img,target,cv2.TM_CCOEFF_NORMED)
     h = target.shape[0]
